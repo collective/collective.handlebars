3f072d82c50e71063f929ca5f5ddcb48ea0cd829
Tom Gross <itconsense@gmail.com>
2016-07-25 09:51:14 +0000

    Back to development: 1.0

--- CHANGES.rst
+++ CHANGES.rst
@@ -2,6 +2,12 @@ Changelog
 =========
 
 
+1.0 (unreleased)
+----------------
+
+- Nothing changed yet.
+
+
 1.0b1 (2016-07-25)
 ------------------
 
--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='collective.handlebars',
-    version='1.0b1',
+    version='1.0.dev0',
     description="Handlebars for Plone",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
