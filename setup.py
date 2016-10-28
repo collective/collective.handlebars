6b9a8727930051e56539c82801de9ecb35429903
Tom Gross <itconsense@gmail.com>
2016-10-28 14:04:28 +0000

    Preparing release 1.1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='collective.handlebars',
-    version='1.1.dev0',
+    version='1.1',
     description="Handlebars for Plone",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
